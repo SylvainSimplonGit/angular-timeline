7828a688fc8ac5501b731f846190905467eab0f8
GRIPPON Sylvain <sylvain.grippon@renault.com>
2020-01-15 09:50:18 +0100

    Add routing and Hidden guessCard

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Card } from '../card';
 import { CardService } from '../card.service';
+import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -12,27 +13,38 @@ export class TimelineComponent implements OnInit {
 
   cardGuess: Card;
   cardsFounded: Card[];
-  nbCardsMax: number;
+  cardsUnfounded: Card[];
+  // nbCardsMax: number;
+  idCurrentTimeline: number;
 
   constructor(
     private cardService: CardService,
+    private route: ActivatedRoute,
   ) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.idCurrentTimeline = +params.get('timelineId'); // le '+' permet de transformer une string en number
+    });
     this.cardGuess = this.cardService.getRandomCard();
     this.cardsFounded = this.cardService.getCardsFounded();
-    this.nbCardsMax = 8;
-
+    this.cardsUnfounded = this.cardService.getCardsUnfounded();
   }
 
   guessCard(card: Card) {
     console.log('guessCard : ');
     console.log('Taille de cardsFounded avant findCard : ' + this.cardsFounded.length);
+    console.log('Taille de cardsUnfounded avant findCard : ' + this.cardsUnfounded.length);
     this.cardService.findCard(card);
     this.cardsFounded = this.cardService.getCardsFounded();
-    if (this.cardsFounded.length != this.nbCardsMax) {
+    if (!this.haveNoCardInUnfounded()) {
       this.cardGuess = this.cardService.getRandomCard();
     }
     console.log('Taille de cardsFounded après findCard : ' + this.cardsFounded.length);
+    console.log('Taille de cardsUnfounded après findCard : ' + this.cardsUnfounded.length);
+  }
+
+  haveNoCardInUnfounded(): boolean {
+    return !(this.cardsUnfounded.length > 0);
   }
 }
